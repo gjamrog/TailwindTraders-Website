54daa07e68f9c0259726b48b94cd59df8ca8a5e3
gjamrog <gjamrog16@gmail.com>
2021-05-01 16:04:50 +0200

    First true commit!

--- qrworker/QrDecoder.py
+++ qrworker/QrDecoder.py
@@ -1,17 +1,22 @@
 import cv2
+from Picasso import Picasso 
 
 class QrDecoder:
     def __init__(self):
         self.decoder = cv2.QRCodeDetector()
+        self.font = cv2.FONT_HERSHEY_COMPLEX
+        self.color = (255,0,0)
     def decode(self,image):
+        font = cv2.FONT_HERSHEY_COMPLEX
         message, points, _ = self.decoder.detectAndDecode(image)
-        cv2.rectangle(image, (points[0][0][0], points[0][0][1]), (points[0][2][0], points[0][2][1]), (255,255,0), 2)
         if(message != ''):
-            print(message)
-        return image
-        
-image = cv2.imread("C:/Users/Lenovo/Documents/QrWorker/resources/helloworld.jpg")
-qrdecoder = QrDecoder()
-cv2.imshow('CHUJ', qrdecoder.decode(image))
-cv2.waitKey(0) 
-cv2.destroyAllWindows()
+            return message, points
+        return None 
+
+if __name__ == "__main__":  
+    image = cv2.imread("C:/Users/Lenovo/Documents/QrWorker/resources/helloworld.jpg")
+    qrdecoder = QrDecoder()
+    a, b = qrdecoder.decode(image)
+    cv2.imshow("penis",  Picasso.drawText(a,b , image )  )
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
